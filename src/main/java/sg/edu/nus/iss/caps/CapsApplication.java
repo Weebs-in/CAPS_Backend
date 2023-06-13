cc4e08b7d9a793e0de6ee803d4849abbd089c050
Cooper <archett0@outlook.com>
2023-06-13 21:16:17 +0800

    Initial Commit

---
+++ src/main/java/sg/edu/nus/iss/caps/CapsApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.caps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapsApplication.class, args);
+    }
+
+}

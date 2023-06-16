bc4cdbeb8ec19492517c3bedf5a915004baa0fe0
Cooper <archett0@outlook.com>
2023-06-16 13:51:20 +0800

    Removed unused imports

--- src/main/java/sg/edu/nus/iss/caps/controller/FacultyController.java
+++ src/main/java/sg/edu/nus/iss/caps/controller/FacultyController.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.iss.caps.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;

5a0d7af8789b8e70e7fae617ef6cba519e0186df
Cooper <archett0@outlook.com>
2023-06-25 00:14:15 +0800

    Remove redundant imports

--- src/main/java/sg/edu/nus/iss/caps/model/LecturerStudentDetails.java
+++ src/main/java/sg/edu/nus/iss/caps/model/LecturerStudentDetails.java
@@ -1,7 +1,5 @@
 package sg.edu.nus.iss.caps.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/sg/edu/nus/iss/caps/repository/LecturerRepository.java
+++ src/main/java/sg/edu/nus/iss/caps/repository/LecturerRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import sg.edu.nus.iss.caps.model.Lecturer;
-import sg.edu.nus.iss.caps.model.LecturerStudentDetails;
-import sg.edu.nus.iss.caps.model.Student;
 
 import java.util.List;
 
